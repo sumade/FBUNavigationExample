02c68d2f724102c8df4ee223c0a6389c6b138bf2
Sumeet Shendrikar <sumeets@fb.com>
2016-06-20 22:11:27 -0700

    Initial Commit

---
+++ NavigationExamples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationExamples
+//
+//  Created by Sumeet Shendrikar on 6/20/16.
+//  Copyright © 2016 Sumeet Shendrikar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
